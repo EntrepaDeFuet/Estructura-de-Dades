473b5129db9da991d60d426ce5c724188fef6817
Oriol Jiménez Blanco <orioljb1999@gmail.com>
2025-02-27 21:56:17 +0000

    ex 6 gairebé acabat

--- Estructura-de-Dades/Pràctica 1/Excercici6/mainExcercici6.cpp
+++ Estructura-de-Dades/Pràctica 1/Excercici6/mainExcercici6.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <stdexcept>
 #include "Peli.cpp"
+#include "Director.cpp"
+#include <fstream>
 using namespace std;
 
+int menu(){
+    int sel;
+    string triar;
+    cin >> triar;
+    try{
+        sel = std::stoi(triar);
+    } catch (std::invalid_argument& e){
+        return 7;
+    }
+
+    return sel;
+}
+
+void afegirDirector(vector <Director> & directors){
+
+    string nom;
+    int id,genere;
+
+    cout << "Quin és el nom del director? " << endl;
+    cin >> nom;
+
+    cout << "Quina és la seva id? " << endl;
+    cin >> id;
+    
+}
+
 int main(){
+
+
     return 0;
 }
