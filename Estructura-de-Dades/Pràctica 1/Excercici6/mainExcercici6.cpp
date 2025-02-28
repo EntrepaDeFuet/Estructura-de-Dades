fc30b3b0106d808d9f93ba0f7502e08cdc3ff626
EntrepaDeFuet <orioljb1999@gmail.com>
2025-02-28 11:55:15 +0100

    Falten mètodes per acabar i fer el main.

--- Estructura-de-Dades/Pràctica 1/Excercici6/mainExcercici6.cpp
+++ Estructura-de-Dades/Pràctica 1/Excercici6/mainExcercici6.cpp
@@ -18,17 +18,140 @@ int menu(){
     return sel;
 }
 
-void afegirDirector(vector <Director> & directors){
+bool directorNoRepetit(vector <Director> &directors, int idActual){
+
+    bool noRepetit = true;
+
+    for (int i=0; i < directors.size() && noRepetit; i++){
+
+        if (directors[i].getDirectorId() == idActual){
+            noRepetit = false;
+        }
+
+    }
+    return noRepetit;
+}
+
+
+void afegirDirector(vector <Director> & directors, vector <int> * pelis){
 
     string nom;
     int id,genere;
 
     cout << "Quin és el nom del director? " << endl;
     cin >> nom;
-
+    
     cout << "Quina és la seva id? " << endl;
     cin >> id;
+
+    if (id < 0){
+
+        throw invalid_argument("La id del director no pot ser negativa." );
+        id = 0000;
+
+    }
+    if (directorNoRepetit(directors,id)){
+
+        cout << "Quin és el seu gènere? (1: Dona, 2: Home, 0: No binari)" << endl;
+        cin >> genere;
+
+        if (genere <0 || genere > 2 ){
+
+            throw invalid_argument ("El gènere que has escollit no està determinat. ");
+            genere = 0;
+        }
+
+        Director directorNou = Director(id, genere, nom);
+        
+        cout << "Voldràs afegir-hi una pel·licula? ('s' = si, 'n' = no)" << endl;
+        char resposta;
+        cin >> resposta;
+        
+        switch (resposta)
+        {
+        case 's':
+            afegirPeliDirectorNou(directorNou, pelis);
+            break;
+
+        case 'n':
+            cout << "Molt bé d'acord." << endl;
+            break;
+        
+        default:
+            cout << "No t'he entès, no s'afegirà cap peli. " << endl;
+            break;
+        }
+
+        directors.push_back(directorNou);
+
+    } else {
+
+        cout << "Ja hi ha un director amb aquesta ID. No es pot afegir un nou director." << endl;
+
+    }
+}
+
+bool peliRepetida(vector <int> * pelis){
+    return true;
+}
+
+void afegirPeliDirectorNou(Director director, vector <int> *pelis){
+
+    string nom;
+    int durada, id;
+    float valoracio;
+
+    cout << "Quin és el nom de la peli? " << endl;
+    cin >> nom;
+
+    cout << "Quina és la durada en minuts de la peli? " << endl;
+    cin >> durada;
     
+    if (durada < 0){
+        throw invalid_argument("La duració de la peli no pot ser negativa.");
+        durada = 90;
+    }
+
+    cout << "Quina és la id de la peli? " << endl;
+    cin >> id;
+    if (id<0){
+        throw invalid_argument ("La id de la peli no pot ser negativa. ");
+        id = 0000;
+    }
+    
+    cout << "Quina és la valoració de la peli ?" << endl;
+    cin >> valoracio;
+    if (valoracio < 0 || valoracio > 10){
+        throw invalid_argument ("La valoració ha d'estar entre 0 i 10. ");
+        valoracio = 5.0;
+    }
+    if (peliRepetida(pelis)){
+
+        cout << "Ja existeix una peli amb aquesta ID, ho sentim no es pot afegir." << endl;
+
+    } else {
+
+        director.addPeli(id,director.getDirectorId(),durada,valoracio,nom);
+        pelis->push_back(id);
+
+    }
+
+}
+
+void eliminarPeli(vector <Director> directors, vector <int> * pelis, int id){
+    bool existeixDirector = false;
+
+    for (int i = 0; i<directors.size() && !existeixDirector ; i++ ){
+        
+        if (directors[i].getDirectorId()==id){
+            existeixDirector = true;
+        }
+
+    }
+
+    if (existeixDirector){
+        
+    }
 }
 
 int main(){
